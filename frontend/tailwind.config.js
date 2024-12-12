f8b3546c88b2bdf646539926568a771fe578fd5c
Joshua Nanninga <jmarknanninga@gmail.com>
2024-12-11 21:05:54 -0600

    frontend established

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+// frontend/tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
